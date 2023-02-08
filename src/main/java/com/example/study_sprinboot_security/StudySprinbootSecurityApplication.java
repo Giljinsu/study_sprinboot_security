3dad9df0ac64b777b647292d567e3f223de472d4
Giljinsu <therin234@gmail.com>
2023-02-08 13:39:28 +0900

    first commit

---
+++ src/main/java/com/example/study_sprinboot_security/StudySprinbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_sprinboot_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySprinbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySprinbootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_sprinboot_security/StudySprinbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_sprinboot_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySprinbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
